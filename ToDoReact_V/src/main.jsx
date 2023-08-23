035cf2eecc19f399a3beecd5349498e0bfcbeb90
GeekyHacks <geekyhacks22@gmail.com>
2023-08-23 17:39:15 +0300

    Make Navbar using vite react setup

---
+++ ToDoReact_V/src/main.jsx
@@ -0,0 +1,11 @@
+import { React, StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+import './styles/Css/App.css';
+import Navbar from './NavBar.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Navbar />
+  </StrictMode>
+);
